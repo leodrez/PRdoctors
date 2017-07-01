796b1769297f981ddf2fff1a2eacb53893bd2472
Leonardo Andres <leonardoandres.1298@gmail.com>
2017-07-01 00:56:15 -0400

    Fakedata added

---
+++ src/data/fakedata.js
@@ -0,0 +1,23 @@
+const doctors = [
+  {
+    'firstName': 'Luis',
+    'lastName': 'Pancho',
+    'speciality': 'Ophtalmologist',
+    'id': '123456789',
+  },
+  {
+    'firstName': 'Leonardo',
+    'lastName': 'Lelele',
+    'speciality': 'Dentist',
+    'id': '987654321',
+  },
+  {
+    'firstName': 'Ariana',
+    'lastName': 'Melendez',
+    'speciality': 'Nurse',
+    'id': '192837465',
+  }
+];
+
+export default doctors;
+  

12dfb323b42298a72a4c44231cebcc4efa9c83c7
Leonardo Andres <leonardoandres.1298@gmail.com>
2017-07-04 23:06:04 -0400

    Redux action

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+export const GET_ALL_DOCTORS
+
+export function getAllDoctors(text) {
+  return {
+    type: GET_ALL_DOCTORS,
+    text
+  }
+}
+
+//dispatch(getAllDoctors(text));
+
+// Doctor selection
+// Location selection
+// Get doctors from API

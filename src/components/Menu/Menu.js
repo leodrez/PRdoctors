00e288e984e0639fcd7a86d075fc4c706e45a372
Leonardo Andres <leonardoandres.1298@gmail.com>
2017-07-10 21:56:06 -0400

    Css color changes

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Link
+} from 'react-router-dom';
 import './Menu.css';
 
 class Menu extends Component {
   render() {
     return (
-      <div className='Menu'>
-        <div className='Menu-body'>
-          <a href='#'>Busqueda</a>
-          <a href='#'>Acerca de</a>
-          <a href='#'>Contactenos</a>
-          <a href='#'>Desarrollador</a>
-          <a href='#'>Disclaimer</a>
-          <a href='#'>Privacy & Terms</a>
+      <BrowserRouter>
+        <div className='Menu'>
+          <div className='Menu-body'>
+            <li><Link to="">Busqueda</Link></li>
+            <li><Link to="">Acerca de</Link></li>
+            <li><Link to="">Contactenos</Link></li>
+            <li><Link to="">Desarrollador</Link></li>
+            <li><Link to="">Disclaimer</Link></li>
+            <li><Link to="">Privacy & Terms</Link></li>
+          </div>
         </div>
-      </div>
+      </BrowserRouter>
     );
   }
 }

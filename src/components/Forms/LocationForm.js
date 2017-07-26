fa6770986398fc846da2d3323edc076b2726300f
Leonardo Andres <leonardoandres.1298@gmail.com>
2017-07-26 19:37:33 -0400

    Form components created

---
+++ src/components/Forms/LocationForm.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class LocationForm extends Component {
+  constructor (props) {
+    super (props); 
+  }
+
+  render () {
+    return (
+      <form>
+        <lable>
+          <input type='text'>
+          
+          </input>
+        </lable>
+      </form>
+    ); 
+  }
+}
+
+export default LocationForm;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import DocForm from '../Forms/DocForm'
+import LocationForm from '../Forms/LocationForm'
 import './Home.css';
 
 class Home extends Component {
@@ -11,11 +13,8 @@ class Home extends Component {
           <h1>Encuentre un doctor.</h1>
         </div>
         <div className='Forms'>
-          <input type='text' value='ingrese especialidad, condicion o 
-          nombre del doctor'></input>
-          <br />
-          <input type='text' value='usar mi localizacion
-          actual'></input>
+          <DocForm />
+          <LocationForm />
         </div>
         <div className='Button'>
           <button className='Submit'>Buscar!</button>

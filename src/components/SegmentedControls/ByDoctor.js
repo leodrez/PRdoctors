189235d09c159051d153e5041d3a68de49905f90
Leonardo Andres <leonardoandres.1298@gmail.com>
2017-08-01 17:32:09 -0400

    By Doctor and By Specialty components added

---
+++ src/components/SegmentedControls/ByDoctor.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './ByDoctor.css';
+
+class ByDoctor extends Component {
+  render() {
+    return (
+      <div className='ByDoctor'>
+        <h1>By Doctor</h1>
+      </div>
+    ); 
+  }
+}
+
+export default ByDoctor;
+
---
+++ src/components/SegmentedControls/BySpecialty.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './BySpecialty.css';
+
+class BySpecialty extends Component {
+  render() {
+    return (
+      <div className='BySpecialty'>
+        <h1>By Specialty</h1>
+      </div>
+    ); 
+  }
+}
+
+export default BySpecialty;
